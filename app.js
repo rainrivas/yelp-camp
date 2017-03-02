c6bf8a16dd6dbe01c363714093795c315a37518e
Rain <rainrivas@gmail.com>
2017-03-01 23:00:39 -0700

    Adding views directory and creating templates for landing page and campgrounds page. Added a hard coded array inside app.js for the campgrounds and created rendering routes to load templates instead of just sending text

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    res.render('landing');
 });
+
+app.get('/campgrounds', function(req, res) {
+    var campgrounds = [{
+        name: "Salmon Creek",
+        image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
+    }, {
+        name: "Joshua Tree",
+        image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
+    }, {
+        name: "Estes Park",
+        image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
+    }];
+    res.render('campgrounds', { campgrounds: campgrounds });
+
+})
 
 app.listen(3000, function() {
     console.log('server running on port 3000');
