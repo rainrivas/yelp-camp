e91ab6ea3c0846eed1cdefa6a344f05c21001ef3
Efrain "Rain <rainrivas@gmail.com>
2017-03-03 12:07:02 -0700

    WIP Post Route Skeletons.

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.get('/', function(req, res) {
-    res.render('landing');
-});
-
-app.get('/campgrounds', function(req, res) {
-    var campgrounds = [{
+var campgrounds = [{
         name: "Salmon Creek",
         image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
     }, {
@@ -18,8 +16,30 @@ app.get('/campgrounds', function(req, res) {
         name: "Estes Park",
         image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
     }];
-    res.render('campgrounds', { campgrounds: campgrounds });
 
+app.get('/', function(req, res) {
+    res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+    res.render('campgrounds', { campgrounds: campgrounds });
+})
+
+app.post("/campgrounds", function(req, res) {
+	// get data from form and add to campgrounds array
+	// add it to our final list of campsites
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {
+		name: name,
+		image: image
+	};
+	campgrounds.push(newCampground);
+	res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req, res) {
+	res.render('new');
 })
 
 app.listen(3000, function() {
