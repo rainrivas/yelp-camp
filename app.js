f2b8576e67e33093f10144a9c2a312901fbba105
Rain <rainrivas@gmail.com>
2017-03-03 22:55:44 -0700

    Corrected route errors. Modified the new.ejs to include a CTA, and installed body-parser dependency

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 
 var campgrounds = [{
-        name: "Salmon Creek",
-        image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-    }, {
-        name: "Joshua Tree",
-        image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-    }, {
-        name: "Estes Park",
-        image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-    }];
+    name: "Salmon Creek",
+    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
+}, {
+    name: "Joshua Tree",
+    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
+}, {
+    name: "Estes Park",
+    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
+}];
 
 app.get('/', function(req, res) {
     res.render('landing');
@@ -26,20 +27,20 @@ app.get('/campgrounds', function(req, res) {
 })
 
 app.post("/campgrounds", function(req, res) {
-	// get data from form and add to campgrounds array
-	// add it to our final list of campsites
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCampground = {
-		name: name,
-		image: image
-	};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+    // get data from form and add to campgrounds array
+    // add it to our final list of campsites
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {
+        name: name,
+        image: image
+    };
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new", function(req, res) {
-	res.render('new');
+    res.render('new');
 })
 
 app.listen(3000, function() {
