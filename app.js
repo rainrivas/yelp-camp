ac6e2b61034a6eb5b449d0df57a69834576e5ee6
Rain <rainrivas@gmail.com>
2017-03-07 19:48:33 -0700

    Commenting out the create method for mongo and setting the campgrounds path to now render based on the db data.

--- app.js
+++ app.js
@@ -29,61 +29,23 @@ Campground.create({
     }
 });
 
-var campgrounds = [{
-    name: "Salmon Creek",
-    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-}, {
-    name: "Joshua Tree",
-    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Estes Park",
-    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Salmon Creek",
-    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-}, {
-    name: "Joshua Tree",
-    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Estes Park",
-    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Salmon Creek",
-    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-}, {
-    name: "Joshua Tree",
-    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Estes Park",
-    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Salmon Creek",
-    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-}, {
-    name: "Joshua Tree",
-    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Estes Park",
-    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Salmon Creek",
-    image: "https://images.pexels.com/photos/7758/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb/"
-}, {
-    name: "Joshua Tree",
-    image: "https://images.pexels.com/photos/128956/pexels-photo-128956.jpeg?h=350&auto=compress&cs=tinysrgb"
-}, {
-    name: "Estes Park",
-    image: "https://images.pexels.com/photos/167701/pexels-photo-167701.jpeg?h=350&auto=compress&cs=tinysrgb"
-}];
-
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {
-        campgrounds: campgrounds
-    });
+    // res.render('campgrounds', {
+    //     campgrounds: campgrounds
+    // });
+
+    // Get all campgrounds from mongodb
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log("error: " + err);
+        } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds })
+        }
+    })
 })
 
 app.post("/campgrounds", function(req, res) {
